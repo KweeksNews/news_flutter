effdac5979f1b41349ad93ef917467993947f3cb
Ahmad Rulim <personal@rulim34.dev>
2022-12-25 14:21:16 +0700

    test: Add search bar filled status notifier test
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

---
+++ test/features/search/presentation/notifier/search_bar_filled_status_notifier_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kweeksnews_app/features/search/presentation/notifier/notifier.dart';
+
+void main() {
+  late ProviderContainer container;
+  late StateNotifierProvider<SearchBarFilledStatusNotifier, bool>
+      searchBarFilledStatusProvider;
+
+  const bool testSearchBarFilledStatus = true;
+
+  setUp(() {
+    container = ProviderContainer();
+    searchBarFilledStatusProvider =
+        StateNotifierProvider<SearchBarFilledStatusNotifier, bool>(
+      (ref) => SearchBarFilledStatusNotifier(),
+    );
+
+    addTearDown(container.dispose);
+  });
+
+  test(
+    'Should set new search bar filled status',
+    () {
+      expect(
+        container.read(searchBarFilledStatusProvider),
+        false,
+      );
+
+      container.read(searchBarFilledStatusProvider.notifier).setFilledStatus(
+            isFilled: testSearchBarFilledStatus,
+          );
+
+      expect(
+        container.read(searchBarFilledStatusProvider),
+        testSearchBarFilledStatus,
+      );
+    },
+  );
+}
